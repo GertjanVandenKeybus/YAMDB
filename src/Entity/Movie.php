e3e38d8ea93cd8ae00bae012f593ed6d4161e1be
gertjanvandenkeybus <gertjan.van.den.keybus@outlook.be>
2022-04-13 00:58:22 +0200

    Add repo and model for movie

---
+++ src/Entity/Movie.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $director;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $release_year;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDirector(): ?string
+    {
+        return $this->director;
+    }
+
+    public function setDirector(string $director): self
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    public function getReleaseYear(): ?int
+    {
+        return $this->release_year;
+    }
+
+    public function setReleaseYear(?int $release_year): self
+    {
+        $this->release_year = $release_year;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
